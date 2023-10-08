8fdc78847e194176f47e974fd7373dcdf5bf505f
Wakio37 <your_email@example.com>
2023-10-08 19:57:10 +0000

    completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,17 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 2:
+        print(items)
+        comma_separated = ", ".join(items[:-1])
+        print (comma_separated)
+        c = f"{comma_separated} and {items[-1]}"
+        print(c)
+        return c
+    if len(items) > 2:
+        print(items)
+        comma_separated = ", ".join(items[:-1])
+        print (comma_separated)
+        c = f"{comma_separated}, and {items[-1]}"
+        print(c)
+        return c
+    else:
+        return items[0]
